c56b5fc8be329a45ad0e064e98259792a0a3f682
Idalis Paz <idalispaz@gmail.com>
2020-06-27 03:42:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
+def sort_array_asc (array)
+  array.sort
+end
 
+
+
+def sort_array_desc (array)
+  array.sort {|x,y| y <=> x }
+end
+
+
+
+def swap_elements (array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+
+
+def reverse_array (array)
+  array.reverse
+end
+
+
+
+def kesha_maker (array)
+  array.collect do |element|
+    element[2] = "$"
+    element
+  end
+end
